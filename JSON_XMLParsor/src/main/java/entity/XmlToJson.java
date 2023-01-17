44ad1cc93981ea310acf234692cd439a4425bf3d
aadi-tomar <randomiitstuff@gmail.com>
2023-01-17 20:39:26 +0530

    XML parsor using jaxb api

--- JSON_XMLParsor/src/main/java/entity/XmlToJson.java
+++ JSON_XMLParsor/src/main/java/entity/XmlToJson.java
@@ -1,8 +1,11 @@
 package entity;
 
+import com.google.gson.GsonBuilder;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.StringReader;
 
 public class XmlToJson {
@@ -22,14 +25,24 @@ public class XmlToJson {
     public static void main(String[] args) throws JAXBException {
         XmlToJson xmlToJson = new XmlToJson();
         xmlToJson.converXMLtoJSON(xmlToJson.xmlString);
+        xmlToJson.readXMLFromFile();
     }
 
-    private void converXMLtoJSON(String xmlString) throws JAXBException {
+    private void converXMLtoJSON( String xmlString) throws JAXBException {
 
         JAXBContext jaxbContext = JAXBContext.newInstance(User.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
         User user = (User) unmarshaller.unmarshal(new StringReader(xmlString));
         System.out.println(user);
+        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(user));
+    }
+
+    private void readXMLFromFile() throws JAXBException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(User.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        User user = (User) unmarshaller.unmarshal(new File("xmlfile.txt"));
+        System.out.println(user);
+
     }
 
 
